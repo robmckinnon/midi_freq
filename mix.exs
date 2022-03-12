423e7df2b69a110914edd7f265fa902cb0ee2b24
robmckinnon <rob@movingflow>
2022-03-12 17:37:58 +0000

    Add CHANGELOG for v0.1.0 release

--- mix.exs
+++ mix.exs
@@ -33,8 +33,7 @@ defmodule MidiFreq.MixProject do
 
   defp package() do
     [
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/robmckinnon/midi_freq"}
     ]
